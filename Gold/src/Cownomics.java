84a548eb8e9bd1e9ae404e3e31935d5d6bbd6107
StormFalcon32 <benchen9708@gmail.com>
2019-01-11 17:02:40 -0500

    Updated Cownomics

--- Gold/src/Cownomics.java
+++ Gold/src/Cownomics.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.StringTokenizer;
