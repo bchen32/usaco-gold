b103be5dbd0b71cfc8363b7ba878a0081421cac7
StormFalcon32 <benchen9708@gmail.com>
2019-02-23 20:10:25 -0500

    Finished Talent

--- Gold/src/Talent.java
+++ Gold/src/Talent.java
@@ -4,12 +4,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Talent {
 
 	public static void main(String[] args) throws IOException {
-//		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\Talent\\1.in"));
+//		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\Talent\\2.in"));
 		BufferedReader in = new BufferedReader(new FileReader("talent.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("talent.out")));
 		StringTokenizer ln = new StringTokenizer(in.readLine());
@@ -17,24 +18,27 @@ public class Talent {
 		int W = Integer.parseInt(ln.nextToken());
 		int[] values = new int[N];
 		int[] weights = new int[N];
+		int maxTalent = 0;
 		for (int i = 0; i < N; i++) {
 			ln = new StringTokenizer(in.readLine());
 			weights[i] = Integer.parseInt(ln.nextToken());
 			values[i] = Integer.parseInt(ln.nextToken());
+			maxTalent += values[i];
 		}
-		int[][] dp = new int[N + 1][W + 1];
-		
-		for (int i = 0; i < N + 1; i++) {
-			for (int j = 0; j < W + 1; j++) {
-				if (i == 0 || j == 0) {
-					dp[i][j] = 0;
-				} else if (weights[i] <= W) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
-				} else {
-					dp[i][j] = dp[i - 1][j];
-				}
+		int[] dp = new int[maxTalent + 1];
+		// Dp[i] is the lowest weight for i talent
+		Arrays.fill(dp, 1 << 30);
+		dp[0] = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = maxTalent; j > values[i] - 1; j--) {
+				dp[j] = Math.min(dp[j], dp[j - values[i]] + weights[i]);
 			}
 		}
+		int best = 0;
+		for (int i = 1; i < maxTalent + 1; i++) {
+			best = (dp[i] >= W) ? Math.max(best, 1000 * i / dp[i]) : best;
+		}
+		out.println(best);
 		out.close();
 		in.close();
 	}
