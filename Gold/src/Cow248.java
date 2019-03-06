c00162c3729790b9f4e9ab8161bcb3b6e36ebb3b
StormFalcon32 <benchen9708@gmail.com>
2019-03-06 17:54:17 -0500

    Started Cow248

---
+++ Gold/src/Cow248.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Cow248 {
+	
+	public static void main(String[] args) throws IOException {
+		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\248\\1.in"));
+		BufferedReader in = new BufferedReader(new FileReader("248.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("248.out")));
+		StringTokenizer tk = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(tk.nextToken());
+		for (int i = 0; i < N; i++) {
+			
+		}
+		out.close();
+		in.close();
+	}
+}
