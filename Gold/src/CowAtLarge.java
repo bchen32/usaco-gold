687c870f86940d2effff7d5b9eac2d246fb003c8
StormFalcon32 <benchen9708@gmail.com>
2019-03-05 21:57:27 -0500

    Finished CowAtLarge

--- Gold/src/CowAtLarge.java
+++ Gold/src/CowAtLarge.java
@@ -4,46 +4,88 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class CowAtLarge {
-
+	
 	LinkedList<Integer>[] adjList;
 	int N;
 	int root;
 	int[] distRoot;
+	int[] distLeaf;
+	int[] parents;
 	boolean[] visited;
-
+	LinkedList<Integer> leaves = new LinkedList<Integer>();
+	
 	public CowAtLarge(LinkedList<Integer>[] a, int n, int k) {
 		adjList = a;
 		N = n;
 		root = k;
 		distRoot = new int[N];
-	}
-
-	public void DFS() {
-		visited = new boolean[N];
-		deepen(0, root);
-	}
-
-	public void deepen(int height, int vertex) {
-		distRoot[vertex] = height;
-		visited[vertex] = true;
-		for (int v : adjList[vertex]) {
-			if (!visited[v]) {
-				deepen(height + 1, v);
+		distLeaf = new int[N];
+		parents = new int[N];
+		for (int i = 0; i < N; i++) {
+			if (adjList[i].size() == 1) {
+				leaves.add(i);
 			}
 		}
 	}
-
-	public void BFS() {
-
+	
+	public void root() {
+		visited = new boolean[N];
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(root);
+		while (!q.isEmpty()) {
+			int curr = q.remove();
+			visited[curr] = true;
+			for (int j : adjList[curr]) {
+				if (!visited[j]) {
+					parents[j] = curr;
+					distRoot[j] = distRoot[curr] + 1;
+					q.add(j);
+				}
+			}
+		}
 	}
-
+	
+	public void leaf() {
+		Arrays.fill(distLeaf, 1 << 30);
+		Queue<Integer> q = new LinkedList<Integer>();
+		for (int i : leaves) {
+			distLeaf[i] = 0;
+			q.add(i);
+			visited[i] = true;
+		}
+		visited = new boolean[N];
+		while (!q.isEmpty()) {
+			int curr = q.remove();
+			visited[curr] = true;
+			for (int j : adjList[curr]) {
+				distLeaf[j] = Math.min(distLeaf[curr] + 1, distLeaf[j]);
+				if (!visited[j]) {
+					q.add(j);
+				}
+			}
+		}
+	}
+	
+	public int solve() {
+		int farmersNeeded = 0;
+		for (int i = 1; i < N; i++) {
+			int parent = parents[i];
+			if (distRoot[parent] < distLeaf[parent] && distLeaf[i] <= distRoot[i]) {
+				farmersNeeded++;
+			}
+		}
+		return farmersNeeded;
+	}
+	
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
-		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\CowAtLarge\\1.in"));
+		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\CowAtLarge\\2.in"));
 		BufferedReader in = new BufferedReader(new FileReader("atlarge.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("atlarge.out")));
 		StringTokenizer tk = new StringTokenizer(in.readLine());
@@ -61,7 +103,9 @@ public class CowAtLarge {
 			adjList[b].add(a);
 		}
 		CowAtLarge calc = new CowAtLarge(adjList, N, K);
-		calc.DFS();
+		calc.root();
+		calc.leaf();
+		out.println(calc.solve());
 		out.close();
 		in.close();
 	}
