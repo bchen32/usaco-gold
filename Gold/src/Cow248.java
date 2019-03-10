58250643d148fb02fb60feb086cbd2d93535f430
StormFalcon32 <benchen9708@gmail.com>
2019-03-09 20:45:14 -0500

    Finished Cow248

--- Gold/src/Cow248.java
+++ Gold/src/Cow248.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
 
 public class Cow248 {
 	
@@ -12,11 +11,29 @@ public class Cow248 {
 		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\248\\1.in"));
 		BufferedReader in = new BufferedReader(new FileReader("248.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("248.out")));
-		StringTokenizer tk = new StringTokenizer(in.readLine());
-		int N = Integer.parseInt(tk.nextToken());
+		int N = Integer.parseInt(in.readLine());
+		int[] nums = new int[N];
 		for (int i = 0; i < N; i++) {
-			
+			nums[i] = Integer.parseInt(in.readLine());
 		}
+		int[][] dp = new int[N][N];
+		// Dp[i][j] is the max for subinterval starting at i, with j + 1 length
+		int ans = 0;
+		for (int l = 0; l < N; l++) {
+			for (int i = 0; i + l < N; i++) {
+				if (l == 0) {
+					dp[i][l] = nums[i];
+					continue;
+				}
+				for (int k = 0; k < l; k++) {
+					if (dp[i][k] == dp[i + k + 1][l - k - 1] && dp[i][k] != 0) {
+						dp[i][l] = Math.max(dp[i][l], dp[i][k] + 1);
+					}
+				}
+				ans = Math.max(ans, dp[i][l]);
+			}
+		}
+		out.println(ans);
 		out.close();
 		in.close();
 	}
