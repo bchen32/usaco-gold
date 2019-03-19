ecc96e35b7e273b648dd46185da68fab8f072b45
StormFalcon32 <benchen9708@gmail.com>
2019-03-18 22:29:12 -0400

    Working on Dream

--- Gold/src/Dream.java
+++ Gold/src/Dream.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Dream {
@@ -15,18 +16,45 @@ public class Dream {
 	private int N;
 	private int M;
 	private int[][] grid;
-	private int[][][] dists;
 	
-	public Dream(int N, int M, int[][] grid, int[][][] dists) {
+	public Dream(int N, int M, int[][] grid) {
 		this.N = N;
 		this.M = M;
 		this.grid = grid;
-		this.dists = dists;
 	}
 	
 	public int solve() {
-		
-		return 0;
+		int[][][] dp = new int[N][M][2];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				dp[i][j][0] = INF;
+				dp[i][j][1] = INF;
+			}
+		}
+		LinkedList<DreamState> q = new LinkedList<DreamState>();
+		while (!q.isEmpty()) {
+			DreamState curr = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int newR = curr.r + dirR[i];
+				int newC = curr.c + dirC[i];
+				if (inRange(newR, newC)) {
+					int newTile = grid[newR][newC];
+					if (newTile != 0 && (newTile != 3 || curr.smell)) {
+						// In bounds and passable
+						boolean smell = (newTile == 2) ? true : (newTile == 4) ? false : curr.smell;
+						if (newTile == 4) {
+							// Slide
+							// Treat slide as a single move but with different dist
+							
+						} else if (curr.dist + 1 < dp[newR][newC][smell ? 1 : 0]) {
+							dp[newR][newC][smell ? 1 : 0] = curr.dist = 1;
+							q.add(new DreamState(newR, newC, smell, curr.dist + 1));
+						}
+					}
+				}
+			}
+		}
+		return Math.min(dp[N - 1][M - 1][0], dp[N - 1][M - 1][1]);
 	}
 	
 	public boolean inRange(int r, int c) {
@@ -42,16 +70,14 @@ public class Dream {
 		int N = Integer.parseInt(tk.nextToken());
 		int M = Integer.parseInt(tk.nextToken());
 		int[][] grid = new int[N][M];
-		int[][][] dists = new int[N][M][2];
+		
 		for (int i = 0; i < N; i++) {
 			tk = new StringTokenizer(in.readLine());
 			for (int j = 0; j < M; j++) {
 				grid[i][j] = Integer.parseInt(tk.nextToken());
-				dists[i][j][0] = INF;
-				dists[i][j][1] = INF;
 			}
 		}
-		Dream solver = new Dream(N, M, grid, dists);
+		Dream solver = new Dream(N, M, grid);
 		int ans = solver.solve();
 		if (ans == Integer.MAX_VALUE) {
 			ans = -1;
@@ -65,14 +91,13 @@ public class Dream {
 class DreamState {
 	int r;
 	int c;
-	int status;
+	boolean smell;
 	int dist;
-	int dir;
 	
-	public DreamState(int r, int c, int status, int dist) {
+	public DreamState(int r, int c, boolean smell, int dist) {
 		this.r = r;
 		this.c = c;
-		this.status = status;
+		this.smell = smell;
 		this.dist = dist;
 	}
 }
