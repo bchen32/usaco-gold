b0b4901fdcc2a16541e87323633e04d4ace20c6a
StormFalcon32 <benchen9708@gmail.com>
2019-03-30 13:43:56 -0400

    Working on MooTube

--- Gold/src/MooTube.java
+++ Gold/src/MooTube.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class MooTube {
@@ -16,10 +17,100 @@ public class MooTube {
 		StringTokenizer tk = new StringTokenizer(in.readLine());
 		int N = Integer.parseInt(tk.nextToken());
 		int Q = Integer.parseInt(tk.nextToken());
+		MTEdge[] edges = new MTEdge[N - 1];
+		MTQuery[] queries = new MTQuery[Q];
 		for (int i = 0; i < N; i++) {
-			
+			tk = new StringTokenizer(in.readLine());
+			int v1 = Integer.parseInt(tk.nextToken()) - 1;
+			int v2 = Integer.parseInt(tk.nextToken()) - 1;
+			int w = Integer.parseInt(tk.nextToken());
+			edges[i] = new MTEdge(v1, v2, w);
 		}
+		for (int i = 0; i < Q; i++) {
+			tk = new StringTokenizer(in.readLine());
+			int threshold = Integer.parseInt(tk.nextToken());
+			int start = Integer.parseInt(tk.nextToken()) - 1;
+			queries[i] = new MTQuery(threshold, start, i);
+		}
+		Arrays.sort(edges);
+		Arrays.sort(queries);
 		out.close();
 		in.close();
+	}
+}
+
+class MTEdge implements Comparable<MTEdge> {
+	public int v1, v2, w;
+	
+	public MTEdge(int v1, int v2, int w) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.w = w;
+	}
+	
+	@Override
+	public int compareTo(MTEdge o) {
+		// TODO Auto-generated method stub
+		return Integer.compare(o.w, w);
+	}
+}
+
+class MTQuery implements Comparable<MTQuery> {
+	public int threshold, start, num;
+	
+	public MTQuery(int threshold, int start, int num) {
+		this.threshold = threshold;
+		this.start = start;
+		this.num = num;
+	}
+	
+	@Override
+	public int compareTo(MTQuery o) {
+		// TODO Auto-generated method stub
+		return Integer.compare(o.threshold, threshold);
+	}
+}
+
+class DisjointSet {
+	int[] arr;
+	int[] size;
+	int N;
+	
+	public DisjointSet(int a) {
+		N = a;
+		arr = new int[N];
+		size = new int[N];
+		Arrays.fill(size, 1);
+		for (int i = 0; i < N; i++) {
+			arr[i] = i;
+		}
+	}
+	
+	public int root(int curr) {
+		while (arr[curr] != curr) {
+			arr[curr] = arr[arr[curr]];
+			curr = arr[curr];
+		}
+		return curr;
+	}
+	
+	public boolean find(int a, int b) {
+		if (root(a) == root(b)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void union(int a, int b) {
+		int aRoot = root(a);
+		int bRoot = root(b);
+		if (size[aRoot] < size[bRoot]) {
+			// Subset B is bigger than subset A, so B should be A's parent
+			arr[aRoot] = arr[bRoot];
+			size[bRoot] += size[aRoot];
+		} else {
+			arr[bRoot] = arr[aRoot];
+			size[aRoot] += size[bRoot];
+		}
 	}
 }
