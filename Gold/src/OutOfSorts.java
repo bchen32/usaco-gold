ebf0cf4391d17028950a8b1314c56cbf136c1fc3
StormFalcon32 <benchen9708@gmail.com>
2019-04-05 21:42:39 -0400

    Started OutOfSorts

---
+++ Gold/src/OutOfSorts.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class OutOfSorts {
+
+	public static void main(String[] args) throws IOException {
+		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\OutOfSorts\\1.in"));
+		// BufferedReader in = new BufferedReader(new FileReader("H:\\git\\USACO-Gold\\Gold\\OutOfSorts\\1.in"));
+		BufferedReader in = new BufferedReader(new FileReader("sort.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sort.out")));
+		StringTokenizer tk = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(tk.nextToken());
+		for (int i = 0; i < N; i++) {
+			
+		}
+		out.close();
+		in.close();
+	}
+}
