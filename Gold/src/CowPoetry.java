a430660f76a0e11731f5cf723a357f9b490ac890
StormFalcon32 <benchen9708@gmail.com>
2019-04-22 20:55:48 -0400

    Finished CowPoetry

--- Gold/src/CowPoetry.java
+++ Gold/src/CowPoetry.java
@@ -4,9 +4,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class CowPoetry {
+	
+	static final long MOD = 1000000007L;
 	
 	public static void main(String[] args) throws IOException {
 		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\CowPoetry\\1.in"));
@@ -22,13 +25,58 @@ public class CowPoetry {
 			tk = new StringTokenizer(in.readLine());
 			words[i] = new CPWord(Integer.parseInt(tk.nextToken()), Integer.parseInt(tk.nextToken()));
 		}
+		HashMap<String, Integer> freqs = new HashMap<String, Integer>();
 		for (int i = 0; i < M; i++) {
-			
+			String rClass = in.readLine();
+			if (freqs.containsKey(rClass)) {
+				freqs.put(rClass, freqs.get(rClass) + 1);
+			} else {
+				freqs.put(rClass, 1);
+			}
 		}
 		// Dp[i] is the number of different ways to make something with i syllables
-		int[] dp = new int[K];
+		long[] dp = new long[K + 1];
+		dp[0] = 1L;
+		// Rhymes[i] is the number of ways to make a line ending with rhyme class i
+		long[] rhymes = new long[N + 1];
+		for (int i = 0; i < K + 1; i++) {
+			for (int j = 0; j < N; j++) {
+				int syllables = words[j].syllables;
+				int rhyme = words[j].rhyme;
+				if (i + syllables > K) {
+					continue;
+				}
+				if (i + syllables == K) {
+					rhymes[rhyme] = (rhymes[rhyme] + dp[i] + MOD) % MOD;
+				}
+				dp[i + syllables] = (dp[i + syllables] + dp[i] + MOD) % MOD;
+			}
+		}
+		long ans = 1L;
+		CowPoetry solve = new CowPoetry();
+		for (String r : freqs.keySet()) {
+			long currClass = 0L;
+			for (int i = 0; i < N + 1; i++) {
+				currClass = (currClass + solve.exp(rhymes[i], freqs.get(r)) + MOD) % MOD;
+			}
+			ans = (ans * currClass + MOD) % MOD;
+		}
+		out.println(ans);
 		out.close();
 		in.close();
+	}
+	
+	public long exp(long b, int p) {
+		if (p == 0) {
+			return 0;
+		} else if (p == 1) {
+			return (b + MOD) % MOD;
+		}
+		long ans = exp(b, p / 2);
+		ans = (ans * ans + MOD) % MOD;
+		if (p % 2 == 1)
+			ans = (ans * b + MOD) % MOD;
+		return (ans + MOD) % MOD;
 	}
 }
 
