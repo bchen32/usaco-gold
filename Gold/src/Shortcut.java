9b2c8e3d1bab5898babd851a841a6be0acd7134b
StormFalcon32 <benchen9708@gmail.com>
2019-05-02 22:41:14 -0400

    Need to debug Shortcut

--- Gold/src/Shortcut.java
+++ Gold/src/Shortcut.java
@@ -16,10 +16,16 @@ public class Shortcut {
 	static final int INF = 1 << 30;
 	
 	int N;
+	int T;
+	int[] numCows;
+	LinkedList<Integer>[] parents;
+	LinkedList<Integer>[] travelRecords;
 	LinkedList<SEdge>[] adjList;
 	
-	public Shortcut(int N, LinkedList<SEdge>[] adjList) {
+	public Shortcut(int N, int T, int[] numCows, LinkedList<SEdge>[] adjList) {
 		this.N = N;
+		this.T = T;
+		this.numCows = numCows;
 		this.adjList = adjList;
 	}
 	
@@ -50,19 +56,86 @@ public class Shortcut {
 			adjList[a].add(new SEdge(b, w));
 			adjList[b].add(new SEdge(a, w));
 		}
-		Shortcut solve = new Shortcut(N, adjList);
-		int[] before = solve.dijkstraAdjList(0);
+		Shortcut solver = new Shortcut(N, T, numCows, adjList);
+		out.println(solver.solve());
 		out.close();
 		in.close();
 	}
 	
-	public int[] dijkstraAdjList(int root) {
+	@SuppressWarnings("unchecked")
+	public int solve() {
+		int[] distBefore = dijkstra(0);
+		travelRecords = new LinkedList[N];
+		for (int i = 0; i < N; i++) {
+			travelRecords[i] = new LinkedList<Integer>();
+		}
+		for (int i = 1; i < N; i++) {
+			LinkedList<Integer> tryPath = new LinkedList<Integer>();
+			tryPath.add(i);
+			LinkedList<Integer> path = calcRecords(i, tryPath);
+			for (int j : path) {
+				travelRecords[j].add(i);
+			}
+		}
+		int bestSaved = 0;
+		for (int i = 1; i < N; i++) {
+			int saved = 0;
+			for (int j : travelRecords[i]) {
+				int distToI = distBefore[i];
+				int saveSingle = distToI - T;
+				saved += saveSingle * numCows[j];
+			}
+			bestSaved = Math.max(bestSaved, saved);
+		}
+		return bestSaved;
+	}
+	
+	public LinkedList<Integer> calcRecords(int curr, LinkedList<Integer> path) {
+		if (curr == 0) {
+			return path;
+		}
+		LinkedList<Integer> best = null;
+		for (int i : parents[curr]) {
+			LinkedList<Integer> tryPath = new LinkedList<Integer>();
+			tryPath.addAll(path);
+			tryPath.add(i);
+			best = compare(best, calcRecords(i, tryPath));
+		}
+		return best;
+	}
+	
+	public LinkedList<Integer> compare(LinkedList<Integer> a, LinkedList<Integer> b) {
+		if (a == null) {
+			return b;
+		}
+		ListIterator<Integer> iterateA = a.listIterator();
+		ListIterator<Integer> iterateB = b.listIterator();
+		while (iterateA.hasNext() && iterateB.hasNext()) {
+			int aNum = iterateA.next();
+			int bNum = iterateB.next();
+			if (aNum < bNum) {
+				return a;
+			}
+			if (bNum < aNum) {
+				return b;
+			}
+		}
+		return a;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public int[] dijkstra(int root) {
 		Queue<SNode> heap = new PriorityQueue<SNode>();
 		int[] dist = new int[N];
 		Arrays.fill(dist, INF);
+		parents = new LinkedList[N];
+		for (int i = 0; i < N; i++) {
+			parents[i] = new LinkedList<Integer>();
+		}
 		boolean[] inSet = new boolean[N];
 		heap.add(new SNode(root, 0));
 		dist[root] = 0;
+		parents[root].add(-1);
 		
 		while (!heap.isEmpty()) {
 			int u = heap.poll().num;
@@ -76,8 +149,13 @@ public class Shortcut {
 				if (!inSet[v]) {
 					if (distThroughU < dist[v]) {
 						dist[v] = distThroughU;
+						parents[v].clear();
+						parents[v].add(u);
 						SNode toAdd = new SNode(v, distThroughU);
 						heap.add(toAdd);
+					} else if (distThroughU == dist[v]) {
+						dist[v] = distThroughU;
+						parents[v].add(u);
 					}
 				}
 			}
