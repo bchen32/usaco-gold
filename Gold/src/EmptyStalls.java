6d994c8a16b68659cc5999441f1792ed6ad90e31
StormFalcon32 <benchen9708@gmail.com>
2019-11-12 21:52:35 -0500

    Finished EmptyStalls
    Running sum keeps track of how many cows are"spilling over"
    Needs 2 passes
    1st pass accumulates to find the "spill over" of cows to index 0
    2nd pass then finds the first spot where running sum <= 0

--- Gold/src/EmptyStalls.java
+++ Gold/src/EmptyStalls.java
@@ -10,7 +10,7 @@ public class EmptyStalls {
 
 	public static void main(String[] args) throws IOException {
 		// BufferedReader in = new BufferedReader(new
-		// FileReader("D:\\Java\\USACO-Gold\\Gold\\EmptyStalls\\1.in"));
+		// FileReader("D:\\Java\\USACO-Gold\\Gold\\EmptyStalls\\5.in"));
 		BufferedReader in = new BufferedReader(new FileReader("empty.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("empty.out")));
 		StringTokenizer tk = new StringTokenizer(in.readLine());
@@ -19,7 +19,30 @@ public class EmptyStalls {
 		int[] stalls = new int[N];
 		for (int i = 0; i < K; i++) {
 			tk = new StringTokenizer(in.readLine());
-
+			int x = Integer.parseInt(tk.nextToken());
+			int y = Integer.parseInt(tk.nextToken());
+			long a = Long.parseLong(tk.nextToken());
+			long b = Long.parseLong(tk.nextToken());
+			for (int j = 1; j <= y; j++) {
+				int ind = (int) ((a * j + b) % N);
+				stalls[ind] += x;
+			}
+		}
+		long runningSum = 0;
+		for (int i = 0; i < N; i++) {
+			runningSum += stalls[i];
+			if (runningSum > 0) {
+				runningSum--;
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			runningSum += stalls[i];
+			if (runningSum > 0) {
+				runningSum--;
+			} else {
+				out.println(i);
+				break;
+			}
 		}
 		out.close();
 		in.close();
