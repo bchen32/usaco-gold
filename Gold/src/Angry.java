300bb761ebd7c97c0326f03590d213d1057782fb
StormFalcon32 <benchen9708@gmail.com>
2020-01-05 20:09:41 -0500

    Nested binary search - resolve 3/10 cases first try had to download test cases for 10/10

--- Gold/src/Angry.java
+++ Gold/src/Angry.java
@@ -7,91 +7,85 @@ import java.io.PrintWriter;
 import java.util.TreeSet;
 
 public class Angry {
-	
+
+	static int N;
+	static TreeSet<Integer> positions;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\Angry\\4.in"));
-		// BufferedReader in = new BufferedReader(new FileReader("angry.in"));
+		// BufferedReader in = new BufferedReader(new
+		// FileReader("D:\\Java\\USACO-Gold\\Gold\\Angry\\4.in"));
+		BufferedReader in = new BufferedReader(new FileReader("angry.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("angry.out")));
-		int N = Integer.parseInt(in.readLine());
-		TreeSet<Long> cows = new TreeSet<Long>();
+		N = Integer.parseInt(in.readLine());
+		positions = new TreeSet<Integer>();
+		TreeSet<Integer> inputs = new TreeSet<Integer>();
 		for (int i = 0; i < N; i++) {
-			cows.add(2L * Integer.parseInt(in.readLine()));
+			inputs.add(2 * Integer.parseInt(in.readLine()));
 		}
-		long maxRad = (cows.last() - cows.first()) / 2;
-		Angry solve = new Angry();
-		out.printf("%.1f\n", solve.bSearch(0, maxRad, cows) / 2.0);
+		int min = inputs.first();
+		for (int i = 0; i < N; i++) {
+			positions.add(inputs.pollFirst() - min);
+		}
+		double ans = bsearchRadius() / 2;
+		String formatted = String.format("%.1f", ans);
+		out.println(formatted);
 		out.close();
 		in.close();
 	}
-	
-	public long bSearch(long l, long h, TreeSet<Long> list) {
-		// Binary search possible radii
-		
-		// If a given radius works, then anything bigger is guaranteed to work and vice versa
-		long currR = (l + h) / 2;
-		while (l < h) {
-			currR = (l + h) / 2;
-			long left = list.first();
-			long right = list.last();
-			// For a given radius, binary search the best point to start
-			// If a given point can reach the left, then anything smaller works
-			// If a given point cannot reach the left, then nothing bigger works
-			while (left < right) {
-				long currStart = (left + right + 1) / 2;
-				if (checkLeft(list, currStart, currR)) {
-					left = currStart;
-				} else {
-					right = currStart - 1;
-				}
-			}
-			// Optimal starting point has been found (furthest right that can still reach left)
-			if (checkRight(list, left, currR)) {
-				h = currR;
+
+	static double bsearchRadius() {
+		int low = 1;
+		int high = positions.last() - positions.first();
+		while (high - low > 1) {
+			int mid = (low + high + 1) / 2;
+			boolean success = bsearchStart(mid);
+			if (!success) {
+				low = mid;
 			} else {
-				l = currR + 1;
+				high = mid;
 			}
 		}
-		return l;
+		return bsearchStart(low) ? low : high;
 	}
-	
-	public boolean checkLeft(TreeSet<Long> list, long start, long r) {
-		// For a given starting pos and radius, check if the explosion can reach all the way left
-		if (start - r <= list.first()) {
-			// Can directly reach
-			return true;
+
+	static boolean bsearchStart(int radius) {
+		int low = positions.first();
+		int high = positions.last();
+		while (high - low > 1) {
+			long mid = (low + high + 1) / 2;
+			if (mid == 1100202307) {
+				System.out.println();
+			}
+			boolean success = checkRight((int) mid, radius);
+			if (!success) {
+				low = (int) mid;
+			} else {
+				high = (int) mid;
+			}
 		}
-		// Find lowest haybale in range
-		long next = list.ceiling(start - r);
-		// Everything is multiplied by two, so radius decrements by 2
-		r -= 2;
-		while (next != list.first()) {
-			if (list.ceiling(next - r) >= next) {
-				// Can't reach anything new
+		return checkLeft(checkRight(low, radius) ? low : high, radius);
+	}
+
+	static boolean checkLeft(int start, int rad) {
+		while (start > positions.first()) {
+			int next = positions.ceiling(start - rad);
+			if (next == start) {
 				return false;
 			}
-			next = list.ceiling(next - r);
-			r -= 2;
+			start = next;
+			rad -= 2;
 		}
 		return true;
 	}
-	
-	public boolean checkRight(TreeSet<Long> list, long start, long r) {
-		// For a given starting pos and radius, check it can reach right
-		if (start + r >= list.last()) {
-			// Can directly reach
-			return true;
-		}
-		// Find largest haybale in range
-		long next = list.floor(start + r);
-		// Everything is multiplied by two, so radius decrements by 2
-		r -= 2;
-		while (next != list.last()) {
-			if (list.floor(next + r) <= next) {
-				// Can't reach new
+
+	static boolean checkRight(int start, int rad) {
+		while (start < positions.last()) {
+			int next = positions.floor(start + rad);
+			if (next == start) {
 				return false;
 			}
-			next = list.floor(next + r);
-			r -= 2;
+			start = next;
+			rad -= 2;
 		}
 		return true;
 	}
