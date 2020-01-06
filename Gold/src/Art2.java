d09b6cb8cd462b5ebc7169e883fa2af4165f51c1
StormFalcon32 <benchen9708@gmail.com>
2020-01-05 21:17:14 -0500

    Resolve - when changing length of array make sure to update fill code

--- Gold/src/Art2.java
+++ Gold/src/Art2.java
@@ -4,49 +4,60 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Stack;
 
 public class Art2 {
-	
+
 	public static void main(String[] args) throws IOException {
-		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\Art\\1.in"));
-		// BufferedReader in = new BufferedReader(new FileReader("H:\\git\\USACO-Gold\\Gold\\Art\\1.in"));
+		// BufferedReader in = new BufferedReader(new
+		// FileReader("D:\\Java\\USACO-Gold\\Gold\\Art2\\2.in"));
 		BufferedReader in = new BufferedReader(new FileReader("art2.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("art2.out")));
 		int N = Integer.parseInt(in.readLine());
 		int[] painting = new int[N + 2];
-		int[] colorStart = new int[N + 1];
-		int[] colorEnd = new int[N + 1];
-		painting[0] = painting[N + 1] = 0;
-		Arrays.fill(colorStart, Integer.MAX_VALUE);
-		Arrays.fill(colorEnd, -1);
-		colorStart[0] = 0;
-		colorEnd[0] = N + 1;
+		int[][] startEnd = new int[N + 1][2];
+		for (int i = 0; i < N + 1; i++) {
+			for (int j = 0; j < 2; j++) {
+				startEnd[i][j] = -1;
+			}
+		}
 		for (int i = 0; i < N; i++) {
 			painting[i + 1] = Integer.parseInt(in.readLine());
-			int color = painting[i + 1];
-			colorStart[color] = Math.min(colorStart[color], i + 1);
-			colorEnd[color] = Math.max(colorEnd[color], i + 1);
 		}
-		
-		Stack<Integer> stack = new Stack<Integer>();
-		int layers = 0;
 		for (int i = 0; i < N + 2; i++) {
-			int currColor = painting[i];
-			if (colorStart[currColor] == i) {
-				stack.push(currColor);
-				layers = Math.max(layers, stack.size());
-			}
-			if (stack.peek() != currColor) {
-				layers = 0;
-				break;
-			}
-			if (colorEnd[currColor] == i) {
-				stack.pop();
+			if (startEnd[painting[i]][0] == -1) {
+				startEnd[painting[i]][0] = i;
+				startEnd[painting[i]][1] = i;
+			} else {
+				startEnd[painting[i]][1] = i;
 			}
 		}
-		out.println(layers - 1);
+		Stack<Integer> stack = new Stack<Integer>();
+		int maxLayers = 0;
+		for (int i = 0; i < N + 2; i++) {
+			int color = painting[i];
+			if (color == 0 && i > 0 && painting[i - 1] != 0) {
+				if (startEnd[painting[i - 1]][1] != i - 1) {
+					maxLayers = 0;
+					break;
+				}
+			}
+			int start = startEnd[color][0];
+			int end = startEnd[color][1];
+			if (i == start) {
+				stack.push(color);
+			}
+			maxLayers = Math.max(maxLayers, stack.size());
+			if (i == end) {
+				if (stack.peek() == color) {
+					stack.pop();
+				} else {
+					maxLayers = 0;
+					break;
+				}
+			}
+		}
+		out.println(maxLayers - 1);
 		out.close();
 		in.close();
 	}
