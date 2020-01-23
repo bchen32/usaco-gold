a10480ba7f4b03c010352207b0408f9f118b42b6
StormFalcon32 <benchen9708@gmail.com>
2020-01-22 21:05:04 -0500

    8/10 Did not do mod correctly, needed to put parentheses on the outside

--- Gold/src/BarnPainting.java
+++ Gold/src/BarnPainting.java
@@ -4,45 +4,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+@SuppressWarnings("unchecked")
 public class BarnPainting {
-	
-	int N;
-	int K;
-	int[] colors;
-	LinkedList<Integer>[] adjList;
-	long[][] dp;
-	
-	static final long MOD = 1000000007L;
-	
-	public BarnPainting(int N, int K, int[] colors, LinkedList<Integer>[] adjList) {
-		this.N = N;
-		this.K = K;
-		this.colors = colors;
-		this.adjList = adjList;
-		dp = new long[N][3];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(dp[i], -1);
-		}
-	}
-	
+
+	static long MOD = 1000000007;
+	static LinkedList<Integer>[] adjList;
+	static int[] colors;
+	static long[][] dp;
+
 	public static void main(String[] args) throws IOException {
-		// BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\bench\\git\\USACO-Gold\\Gold\\BarnPainting\\4.in"));
-		// BufferedReader in = new BufferedReader(new FileReader("H:\\git\\USACO-Gold\\Gold\\BarnPainting\\1.in"));
+		// BufferedReader in = new BufferedReader(new
+		// FileReader("D:\\Java\\USACO-Gold\\Gold\\BarnPainting\\4.in"));
 		BufferedReader in = new BufferedReader(new FileReader("barnpainting.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("barnpainting.out")));
 		StringTokenizer tk = new StringTokenizer(in.readLine());
 		int N = Integer.parseInt(tk.nextToken());
 		int K = Integer.parseInt(tk.nextToken());
-		@SuppressWarnings("unchecked")
-		LinkedList<Integer>[] adjList = new LinkedList[N];
-		int[] colors = new int[N];
-		Arrays.fill(colors, -1);
+		colors = new int[N];
+		dp = new long[N][3];
+		adjList = new LinkedList[N];
 		for (int i = 0; i < N; i++) {
 			adjList[i] = new LinkedList<Integer>();
+			colors[i] = -1;
+			for (int j = 0; j < 3; j++) {
+				dp[i][j] = -1;
+			}
 		}
 		for (int i = 0; i < N - 1; i++) {
 			tk = new StringTokenizer(in.readLine());
@@ -53,43 +42,43 @@ public class BarnPainting {
 		}
 		for (int i = 0; i < K; i++) {
 			tk = new StringTokenizer(in.readLine());
-			int a = Integer.parseInt(tk.nextToken()) - 1;
-			int color = Integer.parseInt(tk.nextToken()) - 1;
-			colors[a] = color;
+			int b = Integer.parseInt(tk.nextToken()) - 1;
+			int c = Integer.parseInt(tk.nextToken()) - 1;
+			colors[b] = c;
 		}
-		BarnPainting solver = new BarnPainting(N, K, colors, adjList);
 		long ans = 0;
-		for (int i = 0; i < 3; i++) {
-			ans += solver.recurse(0, i, -1, -1);
-			ans %= MOD;
+		if (colors[0] == -1) {
+			for (int i = 0; i < 3; i++) {
+				ans = ((ans % MOD) + (recurse(0, -1, i, -1) % MOD)) % MOD;
+			}
+		} else {
+			ans = ((ans % MOD) + (recurse(0, -1, colors[0], -1) % MOD)) % MOD;
 		}
 		out.println(ans);
 		out.close();
 		in.close();
 	}
-	
-	public long recurse(int root, int color, int parent, int prevColor) {
-		if (colors[root] != -1 && (colors[root] != color || colors[root] == prevColor)) {
-			return 0L;
+
+	static long recurse(int barn, int prev, int currColor, int prevColor) {
+		if (colors[barn] != -1 && (colors[barn] != currColor || colors[barn] == prevColor)) {
+			return 0;
 		}
-		if (dp[root][color] != -1) {
-			return dp[root][color];
+		if (dp[barn][currColor] != -1) {
+			return dp[barn][currColor];
 		}
-		long ans = 1;
-		for (int curr : adjList[root]) {
-			if (curr == parent) {
-				continue;
-			}
-			long sub = 0;
-			for (int c = 0; c < 3; c++) {
-				if (c == color) {
-					continue;
+		long overall = 1;
+		for (int adj : adjList[barn]) {
+			if (adj != prev) {
+				long sum = 0;
+				for (int i = 0; i < 3; i++) {
+					if (i != currColor) {
+						sum = ((sum % MOD) + (recurse(adj, barn, i, currColor) % MOD)) % MOD;
+					}
 				}
-				sub += recurse(curr, c, root, color) % MOD;
+				overall = ((overall % MOD) * (sum % MOD)) % MOD;
 			}
-			ans = (ans * sub) % MOD;
 		}
-		dp[root][color] = ans;
-		return ans;
+		dp[barn][currColor] = overall;
+		return overall;
 	}
 }
