910e30285c7f32cba593e09f53a8287d934d863e
StormFalcon32 <benchen9708@gmail.com>
2020-02-01 18:41:22 -0500

    10/10

--- Gold/src/Checklist.java
+++ Gold/src/Checklist.java
@@ -7,89 +7,108 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class Checklist {
-	
-	public CheckCow[] hCheckCows;
-	public CheckCow[] gCheckCows;
-	public int H;
-	public int G;
-	public long[][][] dp;
-	
-	public Checklist(CheckCow[] hCheckCows, CheckCow[] gCheckCows, int H, int G, long[][][] dp) {
-		this.hCheckCows = hCheckCows;
-		this.gCheckCows = gCheckCows;
-		this.H = H;
-		this.G = G;
-		this.dp = dp;
-	}
-	
+
+	static int H;
+	static int G;
+	static Cow[] holsteins;
+	static Cow[] guernseys;
+	static int[][][] dp;
+
 	public static void main(String[] args) throws IOException {
 		// BufferedReader in = new BufferedReader(new
-		// FileReader("C:\\Users\\bjchen\\git\\USACO-Gold\\Gold\\Checklist\\10.in"));
+		// FileReader("D:\\Java\\USACO-Gold\\Gold\\Checklist\\1.in"));
 		BufferedReader in = new BufferedReader(new FileReader("checklist.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("checklist.out")));
-		StringTokenizer ln = new StringTokenizer(in.readLine());
-		int H = Integer.parseInt(ln.nextToken());
-		int G = Integer.parseInt(ln.nextToken());
-		CheckCow[] hCheckCows = new CheckCow[H];
-		CheckCow[] gCheckCows = new CheckCow[G];
-		long[][][] dp = new long[H + 1][G + 1][2];
-		for (int i = 0; i < H + 1; i++) {
-			for (int j = 0; j < G + 1; j++) {
+		StringTokenizer tk = new StringTokenizer(in.readLine());
+		H = Integer.parseInt(tk.nextToken());
+		G = Integer.parseInt(tk.nextToken());
+		holsteins = new Cow[H];
+		guernseys = new Cow[G];
+		dp = new int[H][G][2];
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < G; j++) {
 				for (int k = 0; k < 2; k++) {
 					dp[i][j][k] = -1;
 				}
 			}
 		}
 		for (int i = 0; i < H; i++) {
-			ln = new StringTokenizer(in.readLine());
-			hCheckCows[i] = new CheckCow(Integer.parseInt(ln.nextToken()), Integer.parseInt(ln.nextToken()));
+			tk = new StringTokenizer(in.readLine());
+			int x = Integer.parseInt(tk.nextToken());
+			int y = Integer.parseInt(tk.nextToken());
+			holsteins[i] = new Cow(x, y);
 		}
 		for (int i = 0; i < G; i++) {
-			ln = new StringTokenizer(in.readLine());
-			gCheckCows[i] = new CheckCow(Integer.parseInt(ln.nextToken()), Integer.parseInt(ln.nextToken()));
+			tk = new StringTokenizer(in.readLine());
+			int x = Integer.parseInt(tk.nextToken());
+			int y = Integer.parseInt(tk.nextToken());
+			guernseys[i] = new Cow(x, y);
 		}
-		CheckCow first = hCheckCows[0];
-		Checklist solve = new Checklist(hCheckCows, gCheckCows, H, G, dp);
-		out.println(Math.min(solve.calc(1, -1, 0, first, true), solve.calc(0, 0, 0, first, false)));
+		H--;
+		G--;
+		out.println(calc(0, -1, true));
 		out.close();
 		in.close();
 	}
-	
-	public long calc(int h, int g, long energy, CheckCow prev, boolean isH) {
-		CheckCow curr = (isH) ? hCheckCows[h] : gCheckCows[g];
-		long distSq = (prev.x - curr.x) * (prev.x - curr.x) + (prev.y - curr.y) * (prev.y - curr.y);
-		energy += distSq;
-		if (dp[h + 1][g + 1][((isH) ? 1 : 0)] != -1) {
-			return dp[h + 1][g + 1][((isH) ? 1 : 0)] + energy;
-		}
-		if (h == H - 1 && g == G - 1) {
-			return energy;
-		}
-		long ans;
-		if (h == H - 2) {
-			if (g != G - 1) {
-				ans = calc(h, g + 1, energy, curr, false);
+
+	static int calc(int h, int g, boolean holstein) {
+		int ans = Integer.MAX_VALUE;
+		if (h == H && g == G) {
+			return 0;
+		} else if (g >= 0 && dp[h][g][holstein ? 0 : 1] != -1) {
+			return dp[h][g][holstein ? 0 : 1];
+		} else if (h == H - 1 && g != G) {
+			if (holstein) {
+				// holstein to guernsey
+				int energy = energy(holsteins[h], guernseys[g + 1]);
+				ans = energy + calc(h, g + 1, false);
 			} else {
-				ans = calc(h + 1, g, energy, curr, true);
+				// guernsey to guernsey
+				int energy = energy(guernseys[g], guernseys[g + 1]);
+				ans = energy + calc(h, g + 1, false);
 			}
-		} else {
-			if (g == G - 1) {
-				ans = calc(h + 1, g, energy, curr, true);
+		} else if (g == G) {
+			if (holstein) {
+				// holstein to holstein
+				int energy = energy(holsteins[h], holsteins[h + 1]);
+				ans = energy + calc(h + 1, g, true);
 			} else {
-				ans = Math.min(calc(h + 1, g, energy, curr, true), calc(h, g + 1, energy, curr, false));
+				// guernsey to holstein
+				int energy = energy(guernseys[g], holsteins[h + 1]);
+				ans = energy + calc(h + 1, g, true);
 			}
+		} else if (h != H && g != G && holstein) {
+			// holstein to holstein
+			int energy = energy(holsteins[h], holsteins[h + 1]);
+			ans = Math.min(ans, energy + calc(h + 1, g, true));
+			// holstein to guernsey
+			energy = energy(holsteins[h], guernseys[g + 1]);
+			ans = Math.min(ans, energy + calc(h, g + 1, false));
+		} else if (h != H && g != G) {
+			// guernsey to holstein
+			int energy = energy(guernseys[g], holsteins[h + 1]);
+			ans = Math.min(ans, energy + calc(h + 1, g, true));
+			// guernsey to guernsey
+			energy = energy(guernseys[g], guernseys[g + 1]);
+			ans = Math.min(ans, energy + calc(h, g + 1, false));
 		}
-		dp[h + 1][g + 1][((isH) ? 1 : 0)] = ans - energy;
+		if (g >= 0) {
+			dp[h][g][holstein ? 0 : 1] = ans;
+		}
 		return ans;
 	}
-}
 
-class CheckCow {
-	int x;
-	int y;
-	
-	public CheckCow(int a, int b) {
-		x = a;
-		y = b;
+	static int energy(Cow a, Cow b) {
+		return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+	}
+
+	static class Cow {
+		int x;
+		int y;
+
+		public Cow(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 }
